a081b2430e4b4a60916eb016981fc0492f0de6d9
Christian <paul.chris.dev@gmail.com>
2025-04-07 11:27:06 +0800

    update sync and logging from database

--- src/configs/database.js
+++ src/configs/database.js
@@ -3,5 +3,7 @@ const { Sequelize } = require('sequelize')
 let databaseURL = process.env.DATABASE_URL
 databaseURL = databaseURL ?? 'sqlite::memory:' //just for the simplicity of this activity
 
-exports.database = new Sequelize(databaseURL);
+exports.database = new Sequelize(databaseURL, {
+    logging: true
+});
 
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.listen(PORT, async () => {
   console.log('Server started on port ' + PORT)
   try {
     await database.authenticate();
+    await database.sync()
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
